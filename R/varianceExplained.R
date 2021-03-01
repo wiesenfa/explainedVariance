0484b171911e54d830fdba26d9e61921ce56119d
Manuel Wiesenfarth <m.wiesenfarth@dkfz-heidelberg.de>
2021-03-01 17:58:55 +0100

    CRITICAL: for lme4 only allow uncorrelated REs, changes reading of RE model matrices

--- R/varianceExplained.R
+++ R/varianceExplained.R
@@ -24,32 +24,61 @@ varianceExplained.lmerMod <- varianceExplained.lmerModLmerTest <- function(objec
     lmod <- eval(mc, parent.frame(1L))
     X <-lmod$X[,-1]
     X <- scale(X, center = TRUE, scale = FALSE)
-    Z0 <-lapply(lmod$reTrms$Ztlist,Matrix::t)
-    # check whether both random intercept and random slope for same grouping variable
-      multipleREs <- lapply(object@cnms, function(x) length(x))
+    Z <-lapply(lmod$reTrms$Ztlist,Matrix::t)
+    Z <- lapply(Z, function(x) scale( x, center = TRUE, scale = FALSE))
+    
+    # check whether correltated random intercept and random slope for same grouping variable
+      correlatedREs <- lapply(object@cnms, function(x) length(x)>1)
+      if (any (correlatedREs)) stop("Correlated random effects currently not supported. 
+                                    Use two vertical bars (||) to specify multiple uncorrelated random effects for the same grouping variable, 
+                                    e.g. (x||group)
+                                    or equivantly (1|group) + (0+x|group).")
+    # folgender code war um correlated random effects mit einem | zuszulassen, muss aber angepasst werden  
+    #  Z0 <-lapply(lmod$reTrms$Ztlist,Matrix::t)
+    # # check whether correltated random intercept and random slope for same grouping variable
+    #   multipleREs <- lapply(object@cnms, function(x) length(x))
+    # 
+    # # lme4 combines all random effects of one grouping variable into a single Z, following code separates this
+    #   Z=list()
+    #   j=1
+    #   for (i in 1:length(Z0)){
+    #     if (multipleREs[[i]]==1) {
+    #       Z[[j]] <- Z0[[i]]
+    #       names(Z)[j] <- paste(names(object@cnms)[i],object@cnms[[i]][1], sep=".")
+    #       j=j+1
+    #     }  else {
+    #       for (k in 1: multipleREs[[i]]){
+    #         Z[[j]] <- Z0[[i]][, seq(k,
+    #                                           by=multipleREs[[i]],
+    #                                           length.out=ncol(Z0[[i]])/multipleREs[[i]]) ]
+    #         names(Z)[j] <- paste(names(object@cnms)[i],object@cnms[[i]][k], sep=".")
+    #         j = j +1
+    # 
+    #       }
+    #     }
+    #   }
+    #   Z <- lapply(Z, function(x) scale( x, center = TRUE, scale = FALSE))
+    #   rm(Z0)
 
-    # lme4 combines all random effects of one grouping variable into a single Z, following code separates this
-      Z=list()
-      j=1
-      for (i in 1:length(Z0)){
-        if (multipleREs[[i]]==1) {
-          Z[[j]] <- Z0[[i]]
-          names(Z)[j] <- paste(names(object@cnms)[i],object@cnms[[i]][1], sep=".")
-          j=j+1
-        }  else {
-          for (k in 1: multipleREs[[i]]){
-            Z[[j]] <- Z0[[i]][, seq(k,
-                                              by=multipleREs[[i]],
-                                              length.out=ncol(Z0[[i]])/multipleREs[[i]]) ]
-            names(Z)[j] <- paste(names(object@cnms)[i],object@cnms[[i]][k], sep=".")
-            j = j +1
-
-          }
-        }
-      }
-      Z <- lapply(Z, function(x) scale( x, center = TRUE, scale = FALSE))
-      rm(Z0)
-
+      # alternative funktionen die hilfreich sein könnnen  
+        # reTrms <- mkReTrms(findbars(lme4:::RHSForm(mc$formula)), object@frame)
+        # 
+        # data("Pixel", package="nlme")
+        # mform <- pixel ~ day + I(day^2) + (day || Dog) 
+        # (bar.f <- findbars(mform)) # list with 3 terms
+        # mf <- model.frame(subbars(mform),data=Pixel)
+        # rt <- mkReTrms(bar.f,mf)
+        # names(rt)
+        # expandDoubleVerts(mc$formula)
+        # 
+        # mform <- pixel ~ day + I(day^2) + (day | Dog) 
+        # (bar.f <- findbars(mform)) 
+        # lapply(bar.f,lme4:::mkBlist,fr=mf)
+        # 
+        
+      
+      
+      
   # get variance components
     se2 <-sigma(object)^2
     su<-unlist(lapply(VarCorr(object), function(x) attr(x, "stddev")^2),
@@ -58,9 +87,11 @@ varianceExplained.lmerMod <- varianceExplained.lmerModLmerTest <- function(objec
   # get estimates
     b.hat <- fixef(object)[-1]
     S.b.hat <- vcov(object, full = FALSE)[-1, -1]
-    u.tilde <- unlist(ranef(object), recursive = F)
+    u.tilde <- unlist(ranef(object,condVar = FALSE), recursive = F)
 
-
+  # assign names to Z matrices
+    names(Z) <- names(su)  <- names(u.tilde)
+    
 
   h1 <- compute_h1(Xc = X, Z = Z, su, se2)
 
