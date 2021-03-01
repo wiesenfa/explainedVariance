9a7d055a50cb9fcbd0362fdcada6d12e0709212e
Manuel Wiesenfarth <m.wiesenfarth@dkfz-heidelberg.de>
2021-03-01 12:00:26 +0100

    introduce varExpProp class

--- R/proportionOf.R
+++ R/proportionOf.R
@@ -1,12 +1,18 @@
 proportionOf <- function(object){
-  object$Rx / object$var.y
-  rowSums(object$Rxpart) / object$var.y
-  object$Rz.1 / object$var.y
-  object$Rz.2 / object$var.y
-  object$Rz.pairs / object$var.y
-  object$Rxz / object$var.y
-  object$se2 / object$var.y
-  object$var.y- object$se2 - object$Rx - sum(object$Rz.1) - sum(object$Rz.2) -
-     2 * sum(object$Rz.pairs) - sum(object$Rxz)
-
+  structure(list(
+    Rx = object$Rx / object$var.y,
+    Rxpart = rowSums(object$Rxpart) / object$var.y,
+    Rz.1 = object$Rz.1 / object$var.y,
+    Rz.2 = object$Rz.2 / object$var.y,
+    Rz.pairs = object$Rz.pairs / object$var.y,
+    Rxz = object$Rxz / object$var.y,
+    se2 = object$se2 / object$var.y,
+    error = object$var.y- object$se2 - 
+      object$Rx - 
+      sum(object$Rz.1) - sum(object$Rz.2) -  
+      2 * sum(object$Rz.pairs) - 
+      sum(object$Rxz)
+  ), 
+  class = "varExpProp")
+  
 }
