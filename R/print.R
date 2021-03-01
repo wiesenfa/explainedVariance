96162f904cc3a4f1f05d89fb3d0ab94136d8a789
Manuel Wiesenfarth <m.wiesenfarth@dkfz-heidelberg.de>
2021-03-01 12:11:32 +0100

    update

--- R/print.R
+++ R/print.R
@@ -1,2 +1,3 @@
-print.varExp <-function(x,...)  print(x, ...)
+print.varExp <-function(x,...)  cat("Variance decomposition object. Sum of variance components deviates from var(y) by, ", proportionOf(x)$error,".")
 
+print.varExpProp <-function(x,...)   cat("Proportion of variance explained object. Sum of variance components deviates from var(y) by, ", proportionOf(x)$error/x$var.y*100,"%.")
--- R/summary.R
+++ R/summary.R
@@ -1,1 +1,11 @@
+#' Summary for (proportion of) variance explained by variance components
+#' 
+#' @param object \code{varExp} object created by \code{\link[varianceExplainedPack:varianceExplained]{varianceExplainedPack::varianceExplained()}}.or \code{varExpProp} object created by \code{\link[varianceExplainedPack:proportionOf]{varianceExplainedPack::proportionOf()}}
+#' @export
+#' @rdname summaries
 summary.varExp <-function(x,...)  summary(x, ...)
+
+
+#' @export
+#' @rdname summaries
+summary.varExpProp <-function(x,...)  summary(x, ...)
