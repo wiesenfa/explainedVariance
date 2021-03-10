7e6c02ab4be1c25324e19e0d3d984df2b81e23ed
Manuel Wiesenfarth <m.wiesenfarth@dkfz-heidelberg.de>
2021-03-10 22:24:05 +0100

    print.summary

--- R/print.R
+++ R/print.R
@@ -1,3 +1,51 @@
-print.varExp <-function(x,...)  cat("Variance decomposition object. Sum of variance components deviates from var(y) by, ", proportionOf(x)$error,".")
+#' @param x \code{VarExp} object created by \code{\link[varianceExplainedPack:varianceExplained]{varianceExplainedPack::varianceExplained()}}.or \code{varExpProp} object created by \code{\link[varianceExplainedPack:proportionOf]{varianceExplainedPack::proportionOf()}}
+#' @export
+#' @rdname print.varExp
+print.VarExp <-function(x,...)  cat("Variance decomposition object. Sum of variance components deviates from var(y) by ", x$error,".",sep = "")
 
-print.varExpProp <-function(x,...)   cat("Proportion of variance explained object. Sum of variance components deviates from var(y) by, ", proportionOf(x)$error/x$var.y*100,"%.")
+#' @export
+#' @rdname print.varExp
+print.VarExpProp <-function(x,...)   cat("Proportion of variance explained object. Sum of variance components deviates from var(y) by ", x$error/x$var.y*100,"%.",sep = "")
+
+
+
+
+#' @param x object created by summary()
+#' @export
+#' @rdname print.summary.varExp
+print.summary.VarExp <-function(x,...){
+  cat("Fixed effects:\n")
+  cat("   Total explained variance by fixed effects (and by correlations with random effects):\n")
+  print(x$fixed)
+  cat("   Partial explained variances by fixed effects (excluding by correlations with random effects):\n")
+  print(x$fixedPartial)
+  
+  cat("\nExplained variance by random effects:\n")
+  print(x$random)
+  
+  cat("\nUnexplained variation (residual):\n")
+  print(x$unexplained)
+  cat("\nTotal variation:\n")
+  print(x$total)
+  cat("\nMismatch:\n")
+  print(x$error)
+}
+
+
+#' @export
+#' @rdname print.summary.varExp
+print.summary.VarExpProp <-function(x,...){
+  cat("Fixed effects:\n")
+  cat("   Proportion of total explained variance by fixed effects (and by correlations with random effects):\n")
+  print(x$fixed)
+  cat("   Proportion of partial explained variances by fixed effects (excluding by correlations with random effects):\n")
+  print(x$fixedPartial)
+  
+  cat("\nProportion of explained variance by random effects:\n")
+  print(x$random)
+  
+  cat("\nProportion of unexplained variation (residual):\n")
+  print(x$unexplained)
+}
+
+
