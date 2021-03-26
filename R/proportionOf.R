0732d33cd8b737e4ab1ee5dac9dce300e97cbb82
Manuel Wiesenfarth <m.wiesenfarth@dkfz-heidelberg.de>
2021-03-26 12:28:56 +0100

    proportionOf() gets S3

--- R/proportionOf.R
+++ R/proportionOf.R
@@ -1,8 +1,18 @@
 #' Derive proportion of variance explained by variance components
 #' 
 #' @param object \code{varExp} object created by \code{\link[varianceExplainedPack:varianceExplained]{varianceExplainedPack::varianceExplained()}}.
+#' @param type specify "dataset-specific" or "population" variation in denominator 
 #' @export
-proportionOf <- function(object, type = "dataset-specific"){
+#' @rdname proportionOf
+proportionOf <- function(object,...) UseMethod("proportionOf")
+
+#' @rdname proportionOf
+proportionOf.default <- function(object, ...) stop("not implemented for this class")
+
+
+
+#' @rdname proportionOf
+proportionOf.VarExp <- function(object, type = "dataset-specific"){
   
   if (type != "population") {
     denom <- object$var.y
@@ -27,3 +37,33 @@ proportionOf <- function(object, type = "dataset-specific"){
                )), 
             class = "VarExpProp")
 }
+
+
+#' @rdname proportionOf
+proportionOf.VarExp.boot <- function(object, type = "dataset-specific"){
+  
+  if (type != "population") {
+    denom <- object$var.y
+    res <- list(
+      Rz.2 = object$Rz.2 / denom,
+      Rz.pairs = object$Rz.pairs / denom,
+      Rxz = object$Rxz / denom,
+      error = object$error / denom
+    )
+  }  else {
+    denom <- object$Rx + sum(object$Rz.1) + object$se2
+    res <- NULL
+  }
+  structure(c(res,
+              list(
+                Rx = object$Rx / denom,
+                Rxpart = object$Rxpart / denom,
+                Rz.1 = object$Rz.1 / denom,
+                se2 = object$se2 / denom,
+                var.y = denom,
+                type = type
+              )), 
+            class = "VarExpProp")
+}
+
+
